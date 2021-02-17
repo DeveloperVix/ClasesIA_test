3eb47f82d1094fe370203aff1c741829c97b3a5c
DeveloperVix <vicomisterio@hotmail.com>
2021-02-16 18:51:30 -0600

    Documentación código
    Se documento el código.

--- Agentes/Assets/Scripts/AgenteEstatico.cs
+++ Agentes/Assets/Scripts/AgenteEstatico.cs
@@ -1,6 +1,11 @@
 using System.Collections;
 using UnityEngine;
 
+/*
+    Este código controla la maquina de estados finita del agente, así mismo
+    indica en que estado se encuentra ejecutando únicamente dicho estado
+*/
+
 //Los estados en que puede estar el agente
 public enum AgentState {Idle, OnMovement, Attacking, Dead}
 
@@ -13,66 +18,73 @@ public class AgenteEstatico : MonoBehaviour
     public FunctionsFSM curState;
 
     [Header("Elementos del sensor")]
-    public LayerMask targetMask;
-    public float radiusDetection = 2f;
-    public Transform sensorPosition;
-    public bool targetDetected = false;
+    public LayerMask targetMask;        //Para la detección del target a quien queremos detectar
+    public float radiusDetection = 2f;  //Un radio de detección
+    public Transform sensorPosition;    //La posición del sensor
+    public bool targetDetected = false; //Un variable con dos posibles valores para saber si esta en rango el taget
 
     [Header("Elementos del agente")]
-    public float speedMovement = 5f;
-    public float timeIdle = 10f;
+    public float speedRotation = 5f; //Velocidad de rotación
+    public float timeIdle = 10f;      //Tiempo de espera
 
-    public Vector3[] angles;
-    public int angleIndex;
+    public Vector3[] angles;        //Un arreglo de vectores que guarda los angulos que debe pasar el agente
+    public int angleIndex;          //Una variable para recorrer el arreglo uno por uno
 
 
+    //Declaración de los estados que tendrá el agente para su ejecución
     public readonly IdleStaticState idleState = new IdleStaticState();
     public readonly RotateStaticAgent rotateState = new RotateStaticAgent();
     public readonly AttackingState attackState = new AttackingState();
 
-    // Start is called before the first frame update
+    // Este metodo se manda llamar cuando se ejecuta el proyecto
     void Start()
     {
-        TransitionToState(idleState);
+        TransitionToState(idleState); //Su primer estado es el Idle
     }
 
-    // Update is called once per frame
+    // Este metodo se manda llamar cada frame durante la ejecución del proyecto
     void Update()
     {
-        curState.UpdateState(this);
+        curState.UpdateState(this); //Le estamos dando el metodo de actualización del estado actual del agente
     }
 
+    //Este metodo se utiliza para trabajar con físicas en Unity, se ejecuta cada frame
     private void FixedUpdate()
     {
-        TargetDetected();
+        TargetDetected(); //Metodo para saber si se detecto al juagador, puede ser en cualquier momento dicha detección
     }
 
+    /*
+        se utiliza un sensor para la detección del target haciendo uso de un colisionador que es invisible
+    */
     public virtual void TargetDetected()
     {
-        Collider[] colliders = Physics.OverlapSphere(sensorPosition.position, radiusDetection, targetMask);
-        int i = 0;
-        while(i < colliders.Length)
+        Collider[] colliders = Physics.OverlapSphere(sensorPosition.position, radiusDetection, targetMask); //El colisionador guarda en un arreglo los objetos que este detectando y que tengan el layer
+        if(colliders.Length == 0) //Si no hay objetos en los colisionadore
         {
-            targetDetected = true;
-            i++;
+            targetDetected = false; //No hay target
         }
-        if(colliders.Length == 0)
+        else //Si no
         {
-            targetDetected = false;
+            targetDetected = true; //El target ha sido detectado
         }
     }
 
+    //Metodo para realizar la transicion de un estado a otro, se recibe de parametro el agente
     public void TransitionToState(FunctionsFSM state)
     {
         curState = state;
         curState.EnterState(this);
     }
 
+    //Metodo para ejecutar corutinas y nos permitan ejecutar ciertos procesos en un cantidad de tiempo determinada,
+    //en este caso la espera para depues de rotar
     public void Coroutine(IEnumerator thisCoroutine)
     {
         StartCoroutine(thisCoroutine);
     }
 
+    //Metodo para poder visualizar el colisionador invisible que esta en el metodo TargetDeteted, es solo para orientarse
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
--- Agentes/Assets/Scripts/IdleStaticState.cs
+++ Agentes/Assets/Scripts/IdleStaticState.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class IdleStaticState : FunctionsFSM
 {
+    //Metodo de inicio en el estado Idle
     public override void EnterState(AgenteEstatico agent)
     {
         Debug.Log("Entro a estado Idle");
-        agent.agentStatus = AgentState.Idle;
-        agent.Coroutine(Wait(agent));
+        agent.agentStatus = AgentState.Idle; //Se cambia la variable para saber el estado en que esta el agente
+        agent.Coroutine(Wait(agent));       //Se llama la corutina para iniciar la espera de x segundos
     }
 
+    //Corutina, nos permite hacer una "pausa" antes de ejecutar cierto código
     IEnumerator Wait(AgenteEstatico agent)
     {
-        yield return new WaitForSeconds(agent.speedMovement);
+        yield return new WaitForSeconds(agent.timeIdle); //Espera x segundos, depende del valor de la variable timeIdle
         agent.TransitionToState(agent.rotateState); //Cambia al estado de rotación
     }
 
