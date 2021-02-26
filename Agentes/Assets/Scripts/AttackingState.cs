225553c6dbe7de1b94c8e8a6ac94e3957d2f0d50
DeveloperVix <vicomisterio@hotmail.com>
2021-02-25 23:15:37 -0600

    Estado atacar parte 2
    Esta implementado el estado de atacar, dispara las balas y el auto apuntado hacia el jugador cuando esta en rango de detección.

--- Agentes/Assets/Scripts/AttackingState.cs
+++ Agentes/Assets/Scripts/AttackingState.cs
@@ -4,17 +4,39 @@ using UnityEngine;
 
 public class AttackingState : FunctionsFSM
 {
+    float nextFire;         //Acumular el tiempo para el proximo disparo
+    float rateFire = 1f;    //Cada cuanto tiene que disparar
+
     public override void EnterState(AgenteEstatico agent)
     {
         //Código para cuando entra al estado de atacar
         Debug.Log("Estado de atacar");
         agent.agentStatus = AgentState.Attacking;
 
-        agent.FireBullet();
+        //agent.FireBullet();
+        nextFire = Time.time + rateFire;
     }
 
     public override void UpdateState(AgenteEstatico agent)
     {
-        //Código para cuando no se detecta al jugador
+        if (agent.targetDetected) //Si el target sigue detectado
+        {
+            //Primero hacia que dirección esta el target, hacemos una resta entre las posiciones para obtenerla y guardarla en lookRotation
+            var lookRotation = Quaternion.LookRotation(agent.targetObj.transform.position - agent.transform.position);
+            //Realizaremos una rotación suave a lo largo de la ejecucion, apuntando siempre al target en base a la dirección guardada de lookrotation
+            agent.transform.rotation = Quaternion.Slerp(agent.transform.rotation, lookRotation, agent.speedRotation * Time.deltaTime);
+
+            //Controlamos los disparos, si el tiempo acumulado es menor a Time.time, puede disparar
+            // Time.time es una varible de la librería de Unity, es el tiempo que esta transcurriendo en el juego
+            if (nextFire < Time.time)
+            {
+                agent.FireBullet(); //Se manda llamar el metodo de disparar
+                nextFire = Time.time + rateFire; //acumulamos el tiempo, para cuando toque disparar nuevamente
+            }
+        }
+        else
+        {
+            agent.TransitionToState(agent.idleState);
+        }
     }
 }
